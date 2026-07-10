cebcf10e88460b29b4f730676e61da51a1bc47b4
Ali <alimhassaan442007@gmail.com>
2026-07-11 00:10:05 +0300

    refactor: remove unused import and clean up LoginScreen styling

--- lib/features/login/ui/widgets/login_screen.dart
+++ lib/features/login/ui/widgets/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:doctor_app/core/theming/colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,10 +7,9 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
         title: Text(
           'LoginScreen',
-          style: TextStyle(color: ColorsManager.mainBlue),
+          
         ),
       ),
       body: Center(child: Text('Login Screen')),
