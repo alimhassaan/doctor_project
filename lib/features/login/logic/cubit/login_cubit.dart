acf84731d782dba2f53eebb28fc697c8f134c602
Ali <alimhassaan442007@gmail.com>
2026-07-21 18:04:31 +0300

    feat: implement LoginCubit for managing login states and handling login requests

---
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:doctor_app/core/networking/api_result.dart';
+import 'package:doctor_app/features/login/data/models/login_request_body.dart';
+import 'package:doctor_app/features/login/data/repos/login_repo.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(LoginState.initial());
+  void emitLoginStates(LoginRequestBody loginRequestBody) async {
+    emit(LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+    response.when(
+      success: (loginResponse) {
+        emit(LoginState.success(loginResponse));
+      },
+      failure: (error) {
+        emit(LoginState.error(error: error.apiErrorModel.message ?? ''));
+      },
+    );
+  }
+}
