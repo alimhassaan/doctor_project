fab15dcfe068f388b72362362e7619a21a97f9de
Ali <alimhassaan442007@gmail.com>
2026-07-23 13:44:46 +0300

    feat: add dependency injection setup for ApiService, LoginRepo, and LoginCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:doctor_app/core/di/dependency_injection.dart';
 import 'package:flutter/material.dart';
 import 'package:doctor_app/core/routing/app_router.dart';
 import 'package:doctor_app/doc_app.dart';
 
 void main() {
+  setupGetIt();
   runApp(DocApp(appRouter: AppRouter()));
 }
+
+
