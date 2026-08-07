d7349130b4f0ccb3556c0056acbb905fdfb3d634
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-08-08 02:12:37 +0300

    feat: add main production entry point for application

---
+++ lib/main_production.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/di/dependency_injection.dart';
+import 'package:flutter/material.dart';
+import 'core/routing/app_router.dart';
+import 'doc_app.dart';
+
+void main() async {
+  setupGetIt();
+  // To fix font bug in flutter_screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(appRouter: AppRouter()));
+}
+
+
