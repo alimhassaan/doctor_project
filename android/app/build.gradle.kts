e763d45ce06dff18991b9c07fe8784bb5e7e2934
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-08-07 23:33:38 +0300

    feat: add launch configurations for development and production environments

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.newfolder"
+    namespace = "com.example.doctor_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
---
+++ lib/main_production.dart
@@ -0,0 +1,12 @@
+import 'core/di/dependency_injection.dart';
+import 'package:flutter/material.dart';
+import 'core/routing/app_router.dart';
+import 'doc_app.dart';
+
+void main() {
+  
+  setupGetIt();
+  runApp(DocApp(appRouter: AppRouter()));
+}
+
+
