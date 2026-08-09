8273c90c615de1ca966a5371eab7a5afef91deb3
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-08-10 00:36:14 +0300

    feat: add DoctorsBlueContainer widget with styling and images for home screen

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/features/home/ui/widgets/doctors_blue_container.dart';
 import 'package:doctor_app/features/home/ui/widgets/home_top_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,7 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const HomeTopBar(),
+              const DoctorsBlueContainer(),
             ],
           ),
         ),
---
+++ lib/features/home/ui/widgets/doctors_blue_container.dart
@@ -0,0 +1,63 @@
+import 'package:doctor_app/core/helpers/spacing.dart';
+import 'package:doctor_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DoctorsBlueContainer extends StatelessWidget {
+  const DoctorsBlueContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 195.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 165.h,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24.r),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/home_blue_pattern.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Book and\nschedule with\nnearest doctor',
+                  style: TextStyles.font18WhiteMedium,
+                  textAlign: TextAlign.start,
+                ),
+                verticalSpace(16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(48.r),
+                      ),
+                    ),
+                    child: Text(
+                      'Find Nearby',
+                      style: TextStyles.font12BlueRegular,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 16,
+            bottom: 0,
+            child: Image.asset('assets/images/elmozza.png', height: 195.h),
+          ),
+        ],
+      ),
+    );
+  }
+}
