128d20421cc75ced89a14090496dd4b75888960a
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-08-10 22:10:48 +0300

    refactor: remove unused imports in DoctorsListView widget

--- lib/features/home/ui/widgets/doctors_list_view.dart
+++ lib/features/home/ui/widgets/doctors_list_view.dart
@@ -1,9 +1,7 @@
-import 'package:doctor_app/core/helpers/spacing.dart';
-import 'package:doctor_app/core/theming/styles.dart';
 import 'package:doctor_app/features/home/data/models/specializations_response_model.dart';
 import 'package:doctor_app/features/home/ui/widgets/doctors_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 class DoctorsListView extends StatelessWidget {
   final List<Doctors>? doctorList;
