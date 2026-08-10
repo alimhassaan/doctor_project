1f01368d9fe30809ae5819799caccf518519708f
Ali Mohamed Hassaan <alimhassaan442007@gmail.com>
2026-08-10 23:07:28 +0300

    feat: integrate Specializations and Doctors Bloc Builder into HomeScreen and update dependencies

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,14 +1,9 @@
 import 'package:doctor_app/core/helpers/spacing.dart';
-import 'package:doctor_app/features/home/data/models/specializations_response_model.dart';
-import 'package:doctor_app/features/home/logic/home_cubit.dart';
-import 'package:doctor_app/features/home/logic/home_state.dart';
 import 'package:doctor_app/features/home/ui/widgets/doctors_blue_container.dart';
-import 'package:doctor_app/features/home/ui/widgets/doctors_list_view.dart';
 import 'package:doctor_app/features/home/ui/widgets/doctors_speciality_and_see_all.dart';
-import 'package:doctor_app/features/home/ui/widgets/doctors_speciality_list_view.dart';
 import 'package:doctor_app/features/home/ui/widgets/home_top_bar.dart';
+import 'package:doctor_app/features/home/ui/widgets/specializations_and_doctors_bloc_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -31,52 +26,7 @@ class HomeScreen extends StatelessWidget {
               verticalSpace(16.h),
               const DoctorsSpecialityAndSeeAll(),
               verticalSpace(16.h),
-              BlocBuilder<HomeCubit, HomeState>(
-                buildWhen: (previous, current) =>
-                    current is SpecializationsLoading ||
-                    current is SpecializationsSuccess ||
-                    current is SpecializationsError,
-                builder: (context, state) {
-                  return state.maybeWhen(
-                    specializationsLoading: () {
-                      return const SizedBox(
-                        height: 100,
-                        child: Center(child: CircularProgressIndicator()),
-                      );
-                    },
-                    specializationsSuccess: (specializationsResponseModel) {
-                      final specializationsList =
-                          specializationsResponseModel.specializationDataList
-                                  ?.whereType<SpecializationsData>()
-                                  .toList() ??
-                              const <SpecializationsData>[];
-                      final doctorsList = specializationsList.isNotEmpty
-                          ? specializationsList.first.doctorsList
-                                 ?.whereType<Doctors>()
-                                 .toList() ??
-                             const <Doctors>[]
-                          : const <Doctors>[];
-                      return Expanded(
-                        child: Column(
-                          children: [
-                            DoctorsSpecialityListView(
-                             specializationsDataList: specializationsList,
-                            ),
-                            verticalSpace(8.h),
-                            DoctorsListView(
-                             doctorList: doctorsList,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    specializationsError: (error) {
-                      return SizedBox.shrink();
-                    },
-                    orElse: () => SizedBox.shrink(),
-                  );
-                },
-              ),
+             SpecializationsAndDoctorsBlocBuilder(),
             ],
           ),
         ),
@@ -84,3 +34,8 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
